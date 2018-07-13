2d54a2db5a5be14ec81bbf2ccdd7ab6533c7b8f6
Craig Ellis <craig@Craigs-MacBook-Pro.local>
2018-07-13 09:24:33 +0100

    Default bot created

--- app.js
+++ app.js
@@ -9,4 +9,30 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+// Create chat connector for communicating with the Bot Framework Service
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+server.post('/api/messages', connector.listen());
+
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+var luisAppId = process.env.LuisAppId;
+var luisSubscriptionKey = process.env.LuisAPIKey();
+var luisApiHostName = process.env.luisApiHostName || 'westus.api.cognitive.microsoft.com';
+var luisModelUrl = 'https://' + luisApiHostName + '/luis/v2.0/apps/' + luisAppId + '?subsciptionkey-key=' + luisSubscriptionKey
+
+// Create the default bot session and dialog
+var bot = new builder.UniversalBot(connector, function (session, args) {
+    session.send("Welcome to **Discovery**. The coolest way to discover popular content...");
+});
+
+bot.set('storage', inMemoryStorage);
+
+// Create a recogniser that gets intents from LUIS, and add it to the bot
+var recognizer = new builder.LuisRecognizer(luisModelUrl)
+bot.recognizer(recognizer);
  
