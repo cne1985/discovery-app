72137a04fcd543880625ceec3cd063fec067dfef
Craig Ellis <craig@Craigs-MacBook-Pro.local>
2018-07-13 08:47:02 +0100

    test

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var builder = require('botbuilder');
-console.log("Microsoft Bot Builder SDK installed.");
+var restify = require('restify');
+var discovery = require('./discovery');
+var newsday = require('./newsday');
+
+var server = restify.createServer();
+
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
 
  
